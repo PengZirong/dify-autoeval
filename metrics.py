0732a38e361df060cfe01a510b34a3d5b5993737
Pengzirong <Peng.Zirong@outlook.com>
2024-09-06 16:15:35 +0800

    Refactor metrics.py to add LLMmetrics class for computing metrics

---
+++ metrics.py
@@ -0,0 +1,70 @@
+'''
+Author: Pengzirong Peng.Zirong@outlook.com
+Date: 2024-09-06 14:56:52
+LastEditors: Pengzirong
+LastEditTime: 2024-09-06 16:08:56
+Description: file content
+'''
+
+from typing import List, Dict
+import pandas as pd
+
+
+class LLMmetrics:
+    def __init__(self, evaluation_batch: Dict):
+        self.evaluation_batch = evaluation_batch
+
+    def compute_metrics(self, metrics: List):
+        """Compute metrics using the specified metrics.
+
+        Args:
+            metrics (List): A list of metrics to compute.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the computed metrics.
+        """
+        dataset = pd.DataFrame(self.evaluation_batch)
+        scores = pd.DataFrame()
+        for metric in dataset:
+            score = metric(dataset)
+            scores = scores.append(score)
+            
+        return scores
+    
+    def ragas_metrics(self, ragas_metrics: Dict):
+        """Compute metrics using the ragas library.
+
+        Args:
+            ragas_metrics (Dict): A dictionary containing the metrics for ragas evaluation.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the computed metrics.
+        """
+        from ragas import evaluate
+        ragas_dataset = pd.DataFrame(ragas_metrics)
+        scores = evaluate(ragas_dataset, metrics=ragas_metrics)
+        scores.to_pandas()
+        scores["trace_id"] = self.evaluation_batch["trace_id"]
+        scores["observation_id"] = self.evaluation_batch["observation_id"]
+        return scores
+    
+    def toy_metrics(self, toy_metrics: Dict):
+        """Compute metrics for the toy evaluation batch.
+        example:
+        toy_metrics = {
+            "toy_metric1": random.random,
+            "toy_metric2": random.random,
+            "toy_metric3": random.random,
+            }
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the computed metrics.
+        """
+        # Your code here to compute the metrics for the toy evaluation batch
+        toy_dataset = pd.DataFrame(self.evaluation_batch)
+        for metric in toy_metrics:
+            toy_dataset[metric] = [toy_metrics[metric]() for _ in range(len(toy_dataset))]
+        
+        return toy_dataset
+    
+    
